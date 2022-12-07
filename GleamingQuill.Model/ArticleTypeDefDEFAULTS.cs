c768ccff736b3353ebf3b867e2a60476152b7cdc
Josh Innes <jinnes@pulsion.co.uk>
2022-12-07 16:02:04 +0000

    added default article template

---
+++ GleamingQuill.Model/ArticleTypeDefDEFAULTS.cs
@@ -0,0 +1,17 @@
+﻿using GleamingQuill.Model.ArticleElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GleamingQuill.Model
+{
+    static public class ArticleTypeDefDEFAULTS
+    {
+        static ArticleTypeDef document = new ArticleTypeDef("Document", new List<IArticleElement>()
+        {
+            new LongText()
+        });
+    }
+}
