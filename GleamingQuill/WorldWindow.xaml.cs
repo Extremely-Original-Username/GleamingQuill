ac58aa82446b5d831027007e06d65158cbe491ff
Josh Innes <jinnes@pulsion.co.uk>
2022-12-08 16:34:13 +0000

    began creating article display system (properties need mapped for articles to work)

--- GleamingQuill/WorldWindow.xaml.cs
+++ GleamingQuill/WorldWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using GleamingQuill.Data;
+using GleamingQuill.Model;
 
 namespace GleamingQuill
 {
@@ -28,9 +30,33 @@ namespace GleamingQuill
 
             this.Title = worldName;
 
-            //for future
-            //worldContext = new WorldContext(worldName);
-            //worldContext.Database.EnsureCreated();
+            worldContext = new WorldContext(worldName);
+            worldContext.Database.EnsureCreated();
+
+            StackPanel articlExplorerContentPanel = new StackPanel();
+            articleExplorer.Content = articlExplorerContentPanel;
+
+            foreach (Article article in worldContext.articles)
+            {
+                articlExplorerContentPanel.Children.Add(createArticlePanel(article.Name));
+            }
+        }
+
+        private StackPanel createArticlePanel(string content)
+        {
+            StackPanel panel = new StackPanel()
+            {
+                Width = 240,
+                Height = 50,
+                Margin = new Thickness(5),
+                Background = new SolidColorBrush(Colors.LightGray)
+            };
+
+            panel.Children.Add(
+                new TextBlock() { Text = content }
+            );
+
+            return panel;
         }
     }
 }
